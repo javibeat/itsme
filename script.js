cab8bdf62a3d1b8bf3986cf65702508b15caa8f6
javibeat <javibeat@gmail.com>
2023-12-13 16:44:15 +0400

    navegación teclado

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
         animation: 150,
         chosenClass: "selected",
         dragClass: "drag",
-        delay: 100, // Retraso de 100ms antes de comenzar el arrastre
-        delayOnTouchOnly: true, // Aplica el retraso solo en dispositivos táctiles
-        ghostClass: "ghost", // Clase aplicada al espacio original del elemento arrastrado
+        delay: 100,
+        delayOnTouchOnly: true,
+        ghostClass: "ghost",
         
         onStart: function (evt) {
             evt.item.classList.add('dragging');
@@ -14,5 +14,31 @@ document.addEventListener('DOMContentLoaded', () => {
         onEnd: function (evt) {
             evt.item.classList.remove('dragging');
         }
+    });
+
+    // Asumiendo que '.card' es la clase de tus tarjetas
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.setAttribute('tabindex', '0'); // Hacer que cada tarjeta sea enfocable
+        card.addEventListener('keydown', (e) => {
+            // Obtén el índice de la tarjeta actual enfocada
+            const currentCard = document.activeElement;
+            const currentIndex = Array.from(cards).indexOf(currentCard);
+
+            let nextIndex;
+            if (e.key === 'ArrowRight') {
+                // Enfocar la siguiente tarjeta
+                nextIndex = (currentIndex + 1) % cards.length;
+                cards[nextIndex].focus();
+            } else if (e.key === 'ArrowLeft') {
+                // Enfocar la tarjeta anterior
+                nextIndex = (currentIndex - 1 + cards.length) % cards.length;
+                cards[nextIndex].focus();
+            } else if (e.key === 'Enter') {
+                // Activar algo cuando se presiona Enter, como abrir un enlace
+                currentCard.click();
+            }
+        });
     });
 });
