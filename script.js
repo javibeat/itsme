26fbd4dec9bf5aaaf67dca0b5c3761fbaeebd0c7
javibeat <javibeat@gmail.com>
2023-12-13 15:53:01 +0400

    drag drop sin hueco

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     var el = document.querySelector('.cards-container');
-    var ghost;
-
     var sortable = Sortable.create(el, {
         animation: 150,
         chosenClass: "selected",
         dragClass: "drag",
-        delay: 100, // Retraso de 100ms antes de comenzar el arrastre
-        delayOnTouchOnly: true, // Aplica el retraso solo en dispositivos táctiles
-        ghostClass: "ghost", // Clase aplicada al espacio original del elemento arrastrado
+        delay: 100,
+        delayOnTouchOnly: true,
+        ghostClass: "ghost",
         
         onStart: function (evt) {
             evt.item.classList.add('dragging');
             // Crea un hueco para el elemento arrastrado
-            ghost = document.createElement('div');
+            var ghost = document.createElement('div');
             ghost.classList.add('card', 'ghost');
-            evt.item.parentNode.insertBefore(ghost, evt.item.nextSibling);
-        },
-        onMove: function (evt) {
-            if (ghost && evt.related) {
-                // Mueve el hueco a la nueva posición
-                evt.related.parentNode.insertBefore(ghost, evt.related);
-            }
+            evt.from.insertBefore(ghost, evt.item.nextSibling);
         },
         onEnd: function (evt) {
             evt.item.classList.remove('dragging');
-            // Coloca el elemento arrastrado en la nueva posición y elimina el hueco
+            // Elimina el hueco cuando se suelta el elemento
+            var ghost = document.querySelector('.ghost');
             if (ghost) {
-                ghost.parentNode.insertBefore(evt.item, ghost);
                 ghost.parentNode.removeChild(ghost);
-                ghost = null;
             }
         }
     });
